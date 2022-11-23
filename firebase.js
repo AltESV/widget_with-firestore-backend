ab8a3af889f745cf503fd5c214a8996be43024f3
Daniel <103897412+AltESV@users.noreply.github.com>
2022-11-23 11:06:10 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhfSZR4NQ5cC2h-H_TC3PbuKbB87AqJps",
+  apiKey: "{}",
   authDomain: "sampleimpactdata.firebaseapp.com",
   projectId: "sampleimpactdata",
   storageBucket: "sampleimpactdata.appspot.com",
   messagingSenderId: "56688584270",
-  appId: "1:56688584270:web:f99980423349683e89451f"
+  appId: "{}"
 };
 
 firebase.initializeApp(firebaseConfig);
